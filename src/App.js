5ba93368e2b806ea5ef437641629717df8f22829
NirabhroMakhal <nirabhromakhal@gmail.com>
2023-05-11 22:41:21 +0530

    basic app design main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box, InputAdornment, IconButton, OutlinedInput} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {useState} from "react";
+import colors from "./colors";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const user = window.sessionStorage.getItem('user')
+    if (user === null)
+        window.location.href = "/login";
+
+    const [users, setUsers] = useState([
+        {email: "a"},
+        {email:"b"}
+    ]);
+    const [searchEmail, setSearchEmail] = useState("");
+
+    const [recipient, setRecipient] = useState("");
+    const [message, setMessage] = useState("");
+    const [messageHistory, setmessageHistory] = useState([
+        {
+            from: "nirabhromakhal@gmail.com",
+            to: "a",
+            text: "hi"
+        },
+        {
+            to: "nirabhromakhal@gmail.com",
+            from: "a",
+            text: "I am fine"
+        }
+    ])
+
+    function openChat(email) {
+        setRecipient(email);
+    }
+
+    function handleSearch(email) {
+
+    }
+
+    function sendMessage() {
+
+    }
+
+    return (
+    <Box display="flex" flexDirection="row" width="100vw" height="100vh">
+        <Box display="flex" flexDirection="column" width="30%" height="100%">
+            <Box padding="10px">
+                <OutlinedInput
+                    id="search-box"
+                    type="text"
+                    name="search-box"
+                    size="small"
+                    onChange={event => setSearchEmail(event.target.value)}
+                    placeholder="Search for an user email"
+                    fullWidth
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton onClick={() => handleSearch(searchEmail)} disabled={searchEmail === ""} sx={{
+                                borderRadius: "50%",
+                                '&:hover': {
+                                    backgroundColor: colors.contrastLight
+                                }
+                            }}>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </Box>
+            <Box>
+                {users.map(user => {
+                    return(
+                        <Box padding="10px" onClick={() => {openChat(user.email)}} sx={{
+                            '&:hover': {
+                                backgroundColor: colors.contrastLight
+                            }
+                        }}>{user.email}</Box>
+                    )
+                })}
+            </Box>
+        </Box>
+        <Box display="flex" flexDirection="column" width="70%" height="100%" backgroundColor={colors.primaryLight}>
+            <Box display="flex" flexDirection="column" height="calc(100% - 76px)">
+                {messageHistory.map(message => {
+                    if (message.from === user.email) return(
+                        <Box padding="10px">{message.text}</Box>
+                    )
+                    return (
+                        <Box padding="10px">{message.text}</Box>
+                    )
+                })}
+            </Box>
+            <Box height="76px" padding="10px" boxSizing="border-box">
+                <OutlinedInput
+                    id="search-box"
+                    type="text"
+                    name="search-box"
+                    onChange={event => setMessage(event.target.value)}
+                    placeholder="Send a message"
+                    borderRadius="20px"
+                    fullWidth
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton onClick={() => sendMessage()} disabled={message === ""} sx={{
+                                borderRadius: "50%",
+                                '&:hover': {
+                                    backgroundColor: colors.primary
+                                }
+                            }}>
+                                <SearchIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </Box>
+        </Box>
+    </Box>
+    );
 }
 
 export default App;
---
+++ src/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+    primary: "#9580ff",
+    primaryLight: "#cab0ff",
+    contrast: "#ff9580",
+    contrastLight: "#ffc7b0"
+}
+
+export default colors;
